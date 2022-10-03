d614fe6aafd8e841d5c223b62e59c0fa3d864978
Radi <evaradi18@gmail.com>
2022-10-03 05:34:42 +0100

    Create ImgOrAlt.js

--- src/ImgOrAlt.js
+++ src/ImgOrAlt.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-const ImgOrAlt = ({ src, alt = "Image", ...props }) => {
+const ImgOrAlt = ({ src, placeholder = null, alt = "Image", ...props }) => {
   /*
   This function displays the target image if its available, but
   only displays alt text (without the broken image icon) when the 
   target image isn't available. User can pass in extra props acceptable to the img tag */
-  const altImg = <div>{alt}</div>;
   const altHandler = () => {
-    setImg(altImg);
+    placeholder ? setImg(placeholderJsx) : setImg(altImg);
   };
+
   const imgJsx = (
     <img src={src} alt={alt} onError={altHandler} {...props}></img>
   );
+  const placeholderJsx = <img src={placeholder} alt={alt} {...props}></img>;
+  const altImg = <div>{alt}</div>;
+
   const [Img, setImg] = useState(imgJsx);
+
   return Img;
 };
 
